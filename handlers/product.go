4a7c0aefcdcc4e6d1c877d9660561418a459baca
ArtemMokhov1337 <artemmokhov@mail.ru>
2025-06-09 23:16:46 +0300

    product.go

--- handlers/product.go
+++ handlers/product.go
@@ -1,35 +1,12 @@
-package handlers
+package controllers
 
 import (
+	"crud_api/database"
+	"crud_api/models"
 	"net/http"
-	"strconv"
-
-	"myapi/database"
-	"myapi/models"
 
 	"github.com/gin-gonic/gin"
 )
-
-func GetProducts(c *gin.Context) {
-	var products []models.Product
-
-	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "10"))
-	offset, _ := strconv.Atoi(c.DefaultQuery("offset", "0"))
-
-	database.DB.Limit(limit).Offset(offset).Find(&products)
-	c.JSON(http.StatusOK, products)
-}
-
-func GetProductByID(c *gin.Context) {
-	id := c.Param("id")
-	var product models.Product
-
-	if err := database.DB.First(&product, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Продукт не найден"})
-		return
-	}
-	c.JSON(http.StatusOK, product)
-}
 
 func CreateProduct(c *gin.Context) {
 	var product models.Product
@@ -38,32 +15,48 @@ func CreateProduct(c *gin.Context) {
 		return
 	}
 	database.DB.Create(&product)
-	c.JSON(http.StatusCreated, product)
+	c.JSON(http.StatusOK, product)
+}
+func GetProduct(c *gin.Context) {
+	var products []models.Product
+	database.DB.Find(&products)
+	c.JSON(http.StatusOK, products)
+}
+
+func GetProductByID(c *gin.Context) {
+	id := c.Param("id")
+	var product models.Product
+	if err := database.DB.First(&product, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		return
+	}
+	c.JSON(http.StatusOK, product)
 }
 
 func UpdateProduct(c *gin.Context) {
 	id := c.Param("id")
 	var product models.Product
-
 	if err := database.DB.First(&product, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Продукт не найден"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
 		return
 	}
-
-	if err := c.ShouldBindJSON(&product); err != nil {
+	var input models.Product
+	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
+	product.Name = input.Name
 	database.DB.Save(&product)
 	c.JSON(http.StatusOK, product)
 }
 
 func DeleteProduct(c *gin.Context) {
 	id := c.Param("id")
-	if err := database.DB.Delete(&models.Product{}, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Продукт не найден"})
+	var product models.Product
+	if err := database.DB.First(&product, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Продукт удалён"})
+	database.DB.Delete(&product)
+	c.JSON(http.StatusOK, gin.H{"message": "Product deleted"})
 }
