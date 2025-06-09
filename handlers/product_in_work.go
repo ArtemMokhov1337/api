8cdfda48a7a97640121c465629ab14f4e8971505
ArtemMokhov1337 <artemmokhov@mail.ru>
2025-06-09 23:13:58 +0300

    Update product_in_work.go

--- handlers/product_in_work.go
+++ handlers/product_in_work.go
@@ -3,10 +3,8 @@ package handlers
 import (
 	"net/http"
 	"strconv"
-
 	"myapi/database"
 	"myapi/models"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -44,17 +42,17 @@ func CreateProductInWork(c *gin.Context) {
 func UpdateProductInWork(c *gin.Context) {
 	id := c.Param("id")
 	var piw models.ProductInWork
-
 	if err := database.DB.First(&piw, id).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Запись не найдена"})
 		return
 	}
-
+	var input models.ProductInWork
 	if err := c.ShouldBindJSON(&piw); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
+	piw.UserID = input.UserID
+	piw.ProductID = input.ProductID
 	database.DB.Save(&piw)
 	c.JSON(http.StatusOK, piw)
 }
@@ -65,5 +63,6 @@ func DeleteProductInWork(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Запись не найдена"})
 		return
 	}
+	database.DB.Delete(&piw)
 	c.JSON(http.StatusOK, gin.H{"message": "Запись удалена"})
 }
